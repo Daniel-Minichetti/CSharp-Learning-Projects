123f7b965c27ec18da48da475dc068ec1eaa5208
Daniel Minichetti <daniel.minichetti@gmail.com>
2025-07-14 19:34:12 -0700

    dice-game
    a simple dice game I built that tracks a player and AI's score throwing a dice 10 times. displays appropriate message based on outcome :)

---
+++ dice-game/Dice-Game/Dice-Game/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+
+namespace Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNum;
+            int enemyRandomNum;
+
+            Random val = new Random();
+
+            int playerPoints = 0;
+            int enemyPoints = 0;
+          
+
+            for (int i = 0; i < 10; i++) 
+            {
+                // Print title card 
+                // functions: WriteLine 
+                Console.WriteLine("\nWelcome to the dice game! Press any key to roll the dice");
+                
+                Console.ReadKey();
+                
+                // generates a random between 1 & 6
+                randomNum = val.Next(1, 7);
+                Console.WriteLine($"You rolled a {randomNum}");
+                playerPoints += randomNum;
+                Console.WriteLine($"Current player total is {playerPoints}.");
+
+                // adds tension by waiting 1000ms or 1 second for the enemy AI to display 
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000);
+
+                enemyRandomNum = val.Next(1, 7);
+                Console.WriteLine($"Enemy AI rolled a {enemyRandomNum}");
+                enemyPoints += enemyRandomNum;
+                Console.WriteLine($"Current enemy total is {enemyPoints}.");
+
+            }
+            Console.ReadKey();
+            // write a conditional to declare victor between player and AI 
+            if (playerPoints > enemyPoints)
+            {
+                Console.WriteLine($"player wins with {playerPoints} by {playerPoints - enemyPoints} points!");
+            }
+            else if (enemyPoints > playerPoints)
+            {
+                Console.WriteLine($"enemy AI wins with {enemyPoints} by {enemyPoints - playerPoints} points!");
+            }
+            else
+            {
+                Console.WriteLine($"Both player and AI tied with {playerPoints} & {enemyPoints} respectively.");
+            }
+
+        }
+    }
+}
